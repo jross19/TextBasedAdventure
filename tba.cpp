846af7cd4ef2bd6c3e45d7a988beaeaa52ba6ca0
Jonathan Ross <jross19@k215-01.stjohnsprep.org>
2019-03-26 13:59:00 -0400

    First Commit

---
+++ tba.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+
+using namespace std;
+
+void printInventory(int index, string inventory[])
+{
+	for(int i = 0; i < index; i++)
+	{
+		cout << "(" << i+1 << ")" << inventory[i] << endl;
+	}
+	cout << endl;
+}
+
+void path2(string inventory[])
+{
+	char choice;
+	int choice2;
+	cout << "Do You Want to Take The Chocolate Bar? (y/n): ";
+	cin >> choice;
+	while (!(choice == 'y' || choice == 'Y' || choice == 'n' || choice == 'N'))
+	{
+		cout << "not a valid choice, please reenter: ";
+		cin >> choice;
+		cout << endl;
+	}
+	if (choice == 'y' || choice == 'Y')
+	{
+		cout << "You pick up the chocolate bar" << endl;
+		inventory[1] = "chocolate bar";
+		cout << "Your inventory currently contains:\n";
+		printInventory(2, inventory);
+	}
+	cout << "What would you like to do next?: \n(1) Kill Yourself";
+	cin >> choice2;
+	if (choice2 == 1)
+	{
+		cout << "You point the gun at yourself and end your life quickly" << endl;
+	}
+	
+}
+
+void path(string inventory[])
+{
+	int choice;
+	cout << "You have chosen to continue... you may regret that \n";
+	cout << "You find yourself in front of a gruesome monster, but there is also a chocolate bar next to him \n";
+	cout << "What do you want to do?: \nAttack monster(1)\ntake chocolate bar(2) \n";
+	cin >> choice;
+	while (choice > 2 || choice < 1)
+	{
+		cout << "not a valid choice, please reenter: ";
+		cin >> choice;
+	}
+	if (choice == 1 && inventory[0] == "gun")
+	{
+		cout << "You shoot at the monster, and he dies a slow painful death, and you enjoy it." << endl;
+		path2(inventory);
+	}
+	
+	if (choice == 1 && inventory[0] == "knife")
+	{
+		cout << "You attempt to stab the monster but he guts you before you are able to kill him" << endl;
+		cout << "You slowly bleed out and scream for a total of 69 hours before finally dying" << endl;
+	}
+	
+	if (choice == 2)
+	{
+		cout << "You reach for the chocolate bar, but the monster gets to you first...\nHe eats you for dinner, and has the chocolate as dessert!\n";
+	}
+}
+
+int main()
+{
+	string gun = "gun";
+	string knife = "knife";
+	int choice;
+	int choice2;
+	string inventory[50];
+	cout << "Welcome to The Best Adventure Ever!" << endl;
+	cout << "You Find Yourself in a room, there is a gun and a knife, which one do you want?" << endl;
+	cout << "Knife (1) \n" << "Gun (2): ";
+	cin >> choice;
+	cout << endl;
+	while (choice > 2 || choice < 1)
+	{
+		cout << "not a valid choice, please reenter: ";
+		cin >> choice;
+	}
+	if (choice == 2) 
+	{
+		cout << "You Pick Up The Gun!" << endl;
+		inventory[0] = gun;
+	}
+	if (choice == 1) 
+	{
+		cout << "You Pick Up The Knife!" << endl;
+		inventory[0] = knife;
+	}
+	
+	cout << "What would you like to do next?\nKill Yourself (1)\nContinue (2):";
+	cin >> choice;
+	
+	if (choice == 1 && inventory[0] == gun)
+	{
+		cout << "You point the gun at yourself and end your life quickly" << endl;
+		return 0;
+	}
+	
+	if (choice == 1 && inventory[0] == knife)
+	{
+		cout << "You slowly mutilate yourself before ending your sorry, miserable life." << endl;
+		return 0;
+	}
+	
+	if (choice == 2)
+	{
+		path(inventory);
+		//cout << "You walk off a cliff and fall to your death..." << endl;
+		//cout << "What would";
+		return 0;
+	}
+	
+	
+	
+}
